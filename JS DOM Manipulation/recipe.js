e4d1ca864112bd7f43e73069e239a04dcad8b902
Tariq Moore <66486495+KiddKazz@users.noreply.github.com>
2022-08-29 22:21:05 -0400

    Add files via upload

---
+++ JS DOM Manipulation/recipe.js
@@ -0,0 +1,48 @@
+document.write('<h1>Baked Mac and Cheese</h1>');
+//Main Content of body
+document.write('<main>'); //Main start
+document.write('<p>Everyone loves and knows how to make mac and cheese but, everyone also has their own way of making it.</p>');
+document.write('<p>This recipe is how I make mine which is flexible enough for anyone to change and add their own pizazz to.</p>');
+document.write('<h2><i>Here is what we will need</i></h2>');
+document.write('<ul>');//unordered list start
+document.write('<li><b>Macaroni:</b></li>'); 
+document.write('<ul>'); //mac measurements
+document.write('<li>8oz macaroni (elbow pasta)</li>');
+document.write('<li>1 tbsp of unsalted butter</li>');
+document.write('</ul>'); //mac end
+document.write('<li><b>Topping:</b></li>');
+document.write('<ul>'); //topping measurements
+document.write('<li>(Optional) 2/3 cups of bread crumps</li>');
+document.write('<li> 2 tablespoons of unsalted butter</li>');
+document.write('<li> 1/4 tablespoon of salt</li>');
+document.write('</ul>'); //topping end
+document.write('<li><b>Sauce:</b></li>');
+document.write('<ul>'); //sauce measurements
+document.write('<li>4 tablespoons of unsalted butter</li>');
+document.write('<li>1/3 cup of flour</li>');
+document.write('<li>3 cups of milk</li>');
+document.write('<li>2 cups of shredded cheese</li>');
+document.write('<li>1 cup of shredded mozzarella cheese</li>');
+document.write('<li>3/4 tablespoons of salt</li>')
+document.write('</ul>');//sauce end
+document.write('</ul>');//unordered list end
+//Instructions
+document.write('<h2><i>Instructions</i></h2>');
+document.write('<h3>Pasta:</h3>'); //Pasta steps
+document.write('<ol><li><b>Cook pasta:</b> Bring a large pot of water to boiling. Add macaroni and cook per packet directions MINUS 1 minute.</li>');
+document.write('<li><b>Toss in butter:</b> Drain, return pasta to pot, add butter and toss until melted. Set aside to cool while making the Sauce</li></ol>');
+document.write('<h3>Topping:</h3>'); //Topping steps
+document.write('<ol><li>Mix together toppings and then set aside</li></ol>');
+document.write('<h3>Sauce:</h3>');//Sauce steps
+document.write('<ol><li><b>Preheat oven</b> to 350 degrees fahrenheit.</li>'); 
+document.write('<li><b>Make roux:</b> In a large saucepan, melt butter over medium heat. Add flour and cook, stirring constantly, for 1 minute.</li>');
+document.write('<li><b>Add milk:</b> Add about 1 cup of milk and mix to dissolve the paste into milk.</li>');
+document.write('<li><b>Add Seasonings:</b> Mix in salt and seasonings</li>');
+document.write('<li><b>Thicken sauce:</b>Cook, whisking regularly for about 5-8 minutes, or until a creamy consistency. </li>');
+document.write('<li><b>Add Cheese:</b>Remove from the stove, add cheese and stir.</li>');
+document.write('<li><b>Check salt:</b>Adjust salt to taste, I generally do not put much.</li></ol>');
+document.write('<h2><i>Finally putting it all together!</i></h2>');
+document.write('<ol><li><b>Assemble:</b>Pour sauce into pot with macaroni. Stir quickly, then pour back into skillet. Next I will sprinkle breadcrumbs on top.</li>');
+document.write('<li><b>Bake:</b> for 25 minutes or until top is light golden. Do not bake it too long though!</li></ol>');
+document.write('<h2>Finished!</h2>');
+document.write('</main>'); //Main end */
